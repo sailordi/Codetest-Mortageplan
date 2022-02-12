72ff703a676d2e641e8a1378e73c925d1455daad
Sailordi <sailordi11@gmail.com>
2022-02-12 18:36:42 +0200

    Added a function that performs exp on a number

--- src/main/java/MortageHandler.java
+++ src/main/java/MortageHandler.java
@@ -1,3 +1,16 @@
 /*! The class handles anny mortage functionality */
 public class MortageHandler {
+    public double exp(double num,int exponent){
+        double ret = 1;
+
+            if(exponent != 0) {
+                int t = exponent > 0 ? exponent : (-1)*exponent;
+
+                for(int i = 1; i <= t; i++) {
+                    ret *= num;
+                }
+            }
+            return ret;
+    }
+
 }
