f51c470fe8692cc6f2c5a6ba0339ada602541509
Sailordi <sailordi11@gmail.com>
2022-02-12 17:09:31 +0200

    Added a function that parses a string into a Customer

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -4,4 +4,21 @@ public class DataHandler {
 
     }
 
+    public Customer parseStringToCustomer(String str) {
+        String[] l = str.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*[1-9])");
+
+        if(l.length != 4){
+            return null;
+        }
+
+        int pos = l.length-1;
+
+        int y = Integer.parseInt(l[pos--]);
+        double i = Double.parseDouble(l[pos--]);
+        double t = Double.parseDouble(l[pos--]);
+        String n = l[pos--].replace("\"","");
+
+        return new Customer(n,t,i,y);
+    }
+
 }
